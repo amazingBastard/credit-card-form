d6a5c03572e71a52e7f228edeff213ffdeaa27f0
Damir <damir@withpulp.com>
2015-05-18 03:11:04 -0400

    added more server unit tests to cards model

---
+++ tests/jasmine/server/unit/cards_methods_spec.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('Cards methods', function () {
+
+  it('should insert a new card into the Cards collection', function () {
+
+  });
+});
---
+++ tests/jasmine/server/unit/cards_publication_spec.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('Cards publications', function () {
+
+  it('should only return Cards to their respective owners', function () {
+
+  });
+});
