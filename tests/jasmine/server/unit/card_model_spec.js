3b409c8211c59e1ce77489d9d9022cf8863a116c
Damir <damir@withpulp.com>
2015-05-18 03:31:34 -0400

    fixes to server unit tests

--- tests/jasmine/server/unit/card_model_spec.js
+++ tests/jasmine/server/unit/card_model_spec.js
@@ -2,7 +2,7 @@
 
 describe('Cards model', function() {
 
-  it('is a Mongo collection for saved credit cards', function() {
+  it('is a Mongo collection for user\'s saved credit cards', function() {
 
     // @TODO: tests for allow and helper functions
   });
--- tests/jasmine/server/unit/cards_methods_spec.js
+++ tests/jasmine/server/unit/cards_methods_spec.js
@@ -3,6 +3,8 @@
 describe('Cards methods', function () {
 
   it('should insert a new card into the Cards collection', function () {
+    spyOn(Cards, 'insert');
 
+    //expect(Cards.insert).toHaveBeenCalled();
   });
 });
