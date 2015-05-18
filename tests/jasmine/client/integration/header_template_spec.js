89cf1285a0bbae0da726629a02868e4634e06eaf
Damir <damir@withpulp.com>
2015-05-18 00:03:08 -0400

    added header template spec to client integration tests

---
+++ tests/jasmine/client/integration/header_template_spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('Header template', function() {
+
+	it('should show a link to the about view if the current route is /', function() {
+
+	});
+
+	it('should show a link to the root view if the current route is /about', function() {
+
+	});
+
+	it('should always display the logo', function() {
+		
+	});
+});
--- tests/jasmine/server/unit/card_data_model_spec.js
+++ tests/jasmine/server/unit/card_data_model_spec.js
@@ -1,22 +1,9 @@
 'use strict';
 
-describe('Cards', function() {
+describe('Cards model', function() {
 
   it('is a Mongo collection for saved credit cards', function() {
 
-    //spyOn()
-
+    // @TODO: tests for allow and helper functions
   });
 });
-
-/*
-describe('insertCard', function() {
-  it('should not save if user is not logged in', function() {
-
-  });
-
-  it('should not save if no value was entered', function() {
-
-  });
-});
-*/
